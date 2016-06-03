bcef05b5116e08693def5d45d6321e20b0f65e9d
arico93 <mizuki-ariga@dac.co.jp>
2016-06-03 07:04:12 +0000

    editanddestroy

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,21 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy] 
+  def edit 
+  end
+  
+  def update 
+    if @message.update(message_params)
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else 
+      render 'edit'
+    end 
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
+  
   def index
     @message = Message.new
     @messages = Message.all
@@ -19,5 +36,8 @@ class MessagesController < ApplicationController
   private
   def message_params
     params.require(:message).permit(:name, :body)
+  end
+  def set_message
+    @message = Message.find(params[:id])
   end
 end
